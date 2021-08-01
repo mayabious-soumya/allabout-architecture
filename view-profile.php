34a4d64f63d937ad6b12cbd48b625a54a60aae1e
soumya <saurabh@nettrackers.net>
2021-08-01 08:46:18 +0530

    view profile html

---
+++ view-profile.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <?php include("include/css.php");?> 
+
+    <title>allabout architecture</title>
+  </head>
+  <body>
+     
+    <?php include("include/header.php");?> 
+
+    <section class="inside-page-banner">
+        <img class="img-fluid" src="images/insidepage-banner.png" alt="inside-page">
+    </section>
+
+
+    <section class="view-profile-mainblock">
+
+        <div class="view-profile-topblock">
+            <div class="view-profile-banner">
+                <div class="banner-image">
+                    <img src="images/view-profile-bg.png" alt="">
+                </div>
+            </div>
+
+            <div class="profile-top-details">
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-sm-4">
+                            <div class="user-image">
+                                <img src="images/testimonial1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+
+                            <div class="profile-highlight-block">
+                                <div class="user-name-address-block">
+                                    <h2 class="user-name">Kevin Smith</h2>
+                                    <h4 class="user-designation">Advisor and Consultant at Stripe.in</h4>
+                                    <div class="seller-address">
+                                        <span class="icon"><i class="fas fa-map-marker-alt"></i></span>
+                                        <span class="address">
+                                            555 Californi str, Suite 100, San Francisco, CA 94107
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="green-credits-block">
+                                    <div class="green-credits-box">
+                                        <div class="green-credits-box-header">
+                                            <span>Green Credits</span>
+                                        </div>
+                                        <div class="green-creadit-coins-box">
+                                            <i class="list-icon green-credits"></i>
+                                            <div class="green-creadit-details">
+                                                <p class="text1">22 Super Green Coins</p>
+                                                <p class="text2">Will be credited after return period is over</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="view-profile-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="view-profile-details-left">
+                            
+                            <!----personal details-->
+                            <div class="user-personal-details-list">
+                                <ul class="list-group shadow-none">
+                                    <li class="list-group-item">
+                                        <div class="list-icon"> 
+                                            <i class="fa fa-phone-square"></i>
+                                        </div>
+                                        <div class="list-details"> 
+                                            <span>9910XXXXXX</span> 
+                                            <small>Mobile Number</small>
+                                        </div>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <div class="list-icon"> 
+                                            <i class="fa fa-envelope"></i>
+                                        </div>
+                                        <div class="list-details"> 
+                                            <span>info@example.com</span> 
+                                            <small>Email Address</small>
+                                        </div>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <div class="list-icon"> 
+                                            <i class="fa fa-globe"></i>
+                                        </div>
+                                        <div class="list-details"> 
+                                            <span>www.example.com</span> 
+                                            <small>Website Address</small>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!----personal details--end---->
+
+                            <div class="user-interest-block">
+                                <h3 class="titel"></h3>
+                                <div class="user-interest-list">
+                                    <span class="badge badge-secondary">Interest 1</span>
+                                    <span class="badge badge-secondary">Interest 2</span>
+                                    <span class="badge badge-secondary">Interest 3</span>
+                                    <span class="badge badge-secondary">Interest Name 4</span>
+                                    <span class="badge badge-secondary">Interest Name 5</span>
+                                    <span class="badge badge-secondary">Interest Name 6</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="view-profile-details-right">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="profile-all-details-list">
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="titel"> Manufacturer </td>
+                                                    <td class="description"> Manufacturer </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> Company Name </td>
+                                                    <td class="description"> Company Name</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> About Organisation </td>
+                                                    <td class="description"> About Organisation</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> Genre </td>
+                                                    <td class="description"> Genre</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> Establishment (years) </td>
+                                                    <td class="description"> years </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> Contact Person Name </td>
+                                                    <td class="description"> Full Name</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel">  Contact Person Details </td>
+                                                    <td class="description"> Contact Person Details </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> Last year Turnover </td>
+                                                    <td class="description"> Ammount </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="titel"> Trade Licence </td>
+                                                    <td class="description"> Trade Licence </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td class="titel"> GST Certificate (If available) </td>
+                                                    <td class="description"> GST Number</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td class="titel">  What can you Give to the community </td>
+                                                    <td class="description"> What can you Give to the community</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td class="titel"> Proficiency </td>
+                                                    <td class="description"> Proficiency</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td class="titel">  Education Quaalification (University)</td>
+                                                    <td class="description"> Education Quaalification (University)</td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td class="titel"> What can you need from the Community with? </td>
+                                                    <td class="description"> </td>
+                                                </tr>
+
+                                                                                          
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </section>
+
+
+
+      
+    <?php include("include/footer.php");?> 
+
+	<?php include("include/js.php");?>
+
+
+    
+
+<!----menu--->
+    <script>
+        $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 150) {
+        $(".navwrap").addClass("darkHeader");
+    } else {
+        $(".navwrap").removeClass("darkHeader");
+    }
+});
+    </script>
+
+    <!----menu---end-->
+
+
+
+
+
+  </body>
+</html>
