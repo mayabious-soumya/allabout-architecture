a1a5342fe4ce5806d3794e59d4436c2865ce13f5
soumya <saurabh@nettrackers.net>
2021-08-03 00:12:47 +0530

    faq page

---
+++ faq.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <?php include("include/css.php");?> 
+
+    <title>allabout architecture</title>
+  </head>
+  <body>
+     
+    <?php include("include/header.php");?> 
+
+    <section class="inside-page-banner">
+        <img class="img-fluid" src="images/insidepage-banner.png" alt="inside-page">
+    </section>
+
+
+    <section class="insidepage-content-block faq-main-bkock">
+        <div class="container">
+            <div class="main-titel-block">
+                <h2>FAQ <span>What? Why? How?</span></h2>
+            </div>
+            
+
+            <div class="faq-block">
+                
+                <!--Accordion wrapper-->
+                  <div class="accordion md-accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingOne1">
+                        <a data-toggle="collapse" data-parent="#accordionEx" href="#faq1" aria-expanded="true"
+                           aria-controls="collapseOne1">
+                          <h5 class="mb-0">
+                            1.  What is All About Architecture? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          All About Architecture is a dedicated community to promote sustainable communities and cities. Whether it’s eco-friendly building material, or design technique or any handcrafted sustainable product, this platform showcases everything to make it reach out to wider audiences. It’s a venture to facilitate people in finding out products related to making sustainable communities and helping the modern generation to learn building cities and living sustainably.
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingTwo2">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq2"
+                           aria-expanded="false" aria-controls="collapseTwo2">
+                          <h5 class="mb-0">
+                            2.  What are Green Credits? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                    <div id="faq2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          Every time you make a choice to move towards a Sustainable choice we will Credit you with Green Points. Once you accumulate enough points it can exchanged for many activities in the community
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq3"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            3.  Why should you join the community ? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq3" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          The community grows by spreading knowledge and information related to Sustainable Development. We give you the tools to help you make faster decisions. It’s our aim to build future-ready cities with fresh thinking for sustainable living. To make the process easier, we map the necessary information for all with the help of this platform. Hence, no one will face difficulties while introducing vernacular design techniques.
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq4"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            4.  How can you  make a difference? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq4" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          It’s an open platform where everyone can share their ideas, innovative products, new thinking, and visions related to Sustainable development. Our process starts with an intensive consultation so that together we can make a better world. 
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                     <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq5"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            5.  Who can use this platform? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq5" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          This dedicated space is designed for architects, designers, product manufacturers, builders, real estate developers, small & large scale artisans, planners, interior designers, students, and all other allied professionals involved in the process of building cities. Besides them, the enthusiasts interested in Sustainable development can use this platform to get more information for building smart & sustainable cities. 
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq6"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            6.  Will a builder or real estate developer be benefitted? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq6" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          A real estate developer will find ideas of building sustainable cities by combining traditional and modern design techniques. A developer will find all necessary details here which would help them to avoid wastage of time. 
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq7"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            7.  Will a designer be benefitted? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq7" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          All About Architecture is an evolving platform. Whether you are an architect, or an interior designer, or a planner, you can visit this dedicated platform to upload your project details including images, the scope of work, basic details, design principles, thoughts, and sustainable ideas behind the creation. Moreover, you can showcase your expertise and attract your prospective customers. Request for consultation
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq8"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            8.  Can a product manufacturer use it? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq8" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          If you are an artisan, you can display your handcrafted product here at the ‘Product’ segment. As a result, the designers can get to know about your product who actually look for these items for their projects. This platform acts as a bridge between both of you and helps you get connected to each other. <br><br>
+
+                          Also, a vendor or a manufacturer can showcase his or her product here to reach the targeted customers. Apart from products, you can display the design techniques to make the world know about it.
+
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                    <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq9"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            9.  Is it useful for the students? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq9" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          All About Architecture is the space to share new ideas, techniques, and thinkings for building Sustainable communities. Being a student, you can get associated with this platform to learn new design theories, world-class technologies, and smarter ways to develop cities in the future. You'll find educational videos also to get a lot of interesting info. The result will end up enriching your knowledge as well as understanding.
+
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                     <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq10"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            10. How can you “Give”? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq10" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          You can share your thoughts, information related to vernacular techniques, and sustainable ways to let others know about it. Besides this, you can get endless information and find educational videos on this platform to expand the sphere of your knowledge. 
+
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+                     <!-- Accordion card -->
+                    <div class="card">
+
+                      <!-- Card header -->
+                      <div class="card-header" role="tab" id="headingThree3">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#faq11"
+                           aria-expanded="false" aria-controls="collapseThree3">
+                          <h5 class="mb-0">
+                            11. How can you “Ask”? <i class="fas fa-angle-down rotate-icon"></i>
+                          </h5>
+                        </a>
+                      </div>
+
+                      <!-- Card body -->
+                      <div id="faq11" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+                           data-parent="#accordionEx">
+                        <div class="card-body">
+                          You can ask for a technical help 
+
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <!-- Accordion card -->
+
+
+
+                  </div>
+                  <!-- Accordion wrapper -->
+
+
+
+
+              
+            </div>
+
+
+
+
+
+
+
+        </div>
+    </section>
+
+
+
+      
+    <?php include("include/footer.php");?> 
+
+	<?php include("include/js.php");?>
+
+
+    <script type="text/javascript">
+        var owl = $('.owl-carousel');
+        owl.owlCarousel({
+            items:1,
+            loop:true,
+            margin:0,
+            autoplay:true,
+            autoplayTimeout:5000,
+            autoplayHoverPause:false
+        });
+        $('.play').on('click',function(){
+            owl.trigger('play.owl.autoplay',[1000])
+        })
+        $('.stop').on('click',function(){
+            owl.trigger('stop.owl.autoplay')
+        })
+    </script>
+
+
+    <!----menu--->
+    <script>
+        $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 150) {
+        $(".navwrap").addClass("darkHeader");
+    } else {
+        $(".navwrap").removeClass("darkHeader");
+    }
+});
+    </script>
+
+    <!----menu---end-->
+   
+    </script>
+  </body>
+</html>
